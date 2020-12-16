86647d6ba68abf6767a212de6efd5ddc8a22bd66
Ramil Gilaev <gramil.ul@gmail.com>
2020-12-16 22:23:27 +0400

    Update index key

--- news/news.go
+++ news/news.go
@@ -3,12 +3,10 @@ package news
 import "time"
 
 type News struct {
-	ID          int    `gorm:"primaryKey"`
-	Link        string `gorm:"primaryKey"`
+	ID          int
+	Link        string `sql:"unique_index:idx_link"`
 	Title       string
 	Description string
 	Source      string
 	DatePub     time.Time
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
 }
